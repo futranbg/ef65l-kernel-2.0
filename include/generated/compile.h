f13e5cb549152c72a21f6800345e2b5d3a22a88b
futranbg <nhoc_lau_ca@yahoo.com>
2013-05-28 20:26:34 +0700

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 14 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#14 SMP PREEMPT Tue May 28 18:09:00 ICT 2013"
+#define LINUX_COMPILE_BY "futranbg"
+#define LINUX_COMPILE_HOST "futranbg-VGN-AR730E"
+#define LINUX_COMPILER "gcc version 4.4.5 (Debian 4.4.5-8) "
