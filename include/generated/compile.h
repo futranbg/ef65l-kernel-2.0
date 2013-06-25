38ed086f32502df3a48cb52a522a50195837f1bb
futranbg <nhoc_lau_ca@yahoo.com>
2013-06-25 21:20:50 +0700

    add smartassH3 governor 	modified:   .config 	modified:   .version 	modified:   Module.symvers 	modified:   drivers/cpufreq/Kconfig 	modified:   drivers/cpufreq/Makefile 	modified:   include/config/auto.conf 	modified:   include/config/kernel.release 	modified:   include/config/tristate.conf 	modified:   include/generated/autoconf.h 	modified:   include/generated/compile.h 	modified:   include/generated/utsrelease.h 	modified:   include/linux/cpufreq.h 	drivers/cpufreq/cpufreq_smartassH3.c

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 112 */
+/* This file is auto generated, version 113 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#112 SMP PREEMPT Tue Jun 25 13:21:35 ICT 2013"
+#define UTS_VERSION "#113 SMP PREEMPT Tue Jun 25 20:53:06 ICT 2013"
 #define LINUX_COMPILE_BY "Futranbg"
 #define LINUX_COMPILE_HOST "visdroid"
 #define LINUX_COMPILER "gcc version 4.7.4 20130603 (prerelease) (crosstool-NG hg+default-81ead21f86c2 - Linaro GCC 2013.06 - Cortex-A8) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.8-performance-g546499b-dirty"
+#define UTS_RELEASE "3.0.8-performance-g235dee3-dirty"
