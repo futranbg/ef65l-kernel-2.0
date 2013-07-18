1b1490b8b0b4447004197ce1105a0ca86fd16883
futranbg <nhoc_lau_ca@yahoo.com>
2013-07-18 10:17:27 +0700

    acpuclock reconfig for stable

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 141 */
+/* This file is auto generated, version 142 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#141 SMP PREEMPT Sat Jun 29 23:26:34 ICT 2013"
+#define UTS_VERSION "#142 SMP PREEMPT Sun Jul 14 11:16:44 ICT 2013"
 #define LINUX_COMPILE_BY "Futranbg"
 #define LINUX_COMPILE_HOST "visdroid"
 #define LINUX_COMPILER "gcc version 4.7.4 20130603 (prerelease) (crosstool-NG hg+default-81ead21f86c2 - Linaro GCC 2013.06 - Cortex-A8) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.8-performance-g92d1896-dirty"
+#define UTS_RELEASE "3.0.8-performance-g8fdc049-dirty"
